f73e622b217e89866270272a7b99b912feaa40c1
AshtonHamlin <aghamlin09@gmail.com>
2026-07-23 08:48:40 -0900

    Refactor motor speed assignments for movement commands

--- robot_move_real_ash.py
+++ robot_move_real_ash.py
@@ -7,71 +7,71 @@ def robo_movement(movement):
     move = movement
     if move == "forward":
         # Forward: all motors forward
-        m1 += motor_speed
-        m2 += motor_speed
-        m3 += motor_speed
-        m4 += motor_speed
+        m1 = motor_speed
+        m2 = motor_speed
+        m3 = motor_speed
+        m4 = motor_speed
 
     if move == "backward":
         # Backward: all motors backward
-        m1 -= motor_speed
-        m2 -= motor_speed
-        m3 -= motor_speed
-        m4 -= motor_speed
+        m1 = -motor_speed
+        m2 = -motor_speed
+        m3 = -motor_speed
+        m4 = -motor_speed
 
     if move == "left":
         # Left strafe: top-left and back-right backward, top-right and back-left forward
-        m1 -= motor_speed
-        m2 += motor_speed
-        m3 += motor_speed
-        m4 -= motor_speed
+        m1 = -motor_speed
+        m2 = motor_speed
+        m3 = motor_speed
+        m4 = -motor_speed
 
     if move == "right":
         # Right strafe: top-left and back-right forward, top-right and back-left backward
-        m1 += motor_speed
-        m2 -= motor_speed
-        m3 -= motor_speed
-        m4 += motor_speed
+        m1 = motor_speed
+        m2 = -motor_speed
+        m3 = -motor_speed
+        m4 = motor_speed
 
     if move == "top_left":
         m1 = 0
-        m2 += motor_speed
-        m3 += motor_speed
+        m2 = motor_speed
+        m3 = motor_speed
         m4 = 0
 
     if move == "bottom_right":
         m1 = 0
-        m2 -= motor_speed
-        m3 -= motor_speed
+        m2 = -motor_speed
+        m3 = -motor_speed
         m4 = 0
 
     if move == "bottom_left":
-        m1 -= motor_speed
+        m1 = -motor_speed
         m2 = 0
         m3 = 0
-        m4 -= motor_speed
+        m4 = -motor_speed
 
     if move == "top_right":
-        m1 += motor_speed
+        m1 = motor_speed
         m2 = 0
         m3 = 0
-        m4 += motor_speed
+        m4 = motor_speed
 
     # Handle turning
     if move == "turn_left":
         # Turn left: counterclockwise rotation
-        m1 -= turn_speed
-        m2 -= turn_speed
+        m1 = -turn_speed
+        m2 = -turn_speed
         m3 += turn_speed
         m4 += turn_speed
 
     if move == "turn_right":
         # Turn right: clockwise rotation
-        m1 += turn_speed
-        m2 += turn_speed
-        m3 -= turn_speed
-        m4 -= turn_speed
-
+        m1 = turn_speed
+        m2 = turn_speed
+        m3 = -turn_speed
+        m4 = -turn_speed
+    #stop
     if move == "stop":
         m1 = 0
         m2 = 0
